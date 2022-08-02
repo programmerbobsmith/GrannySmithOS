4957cb86b3abb51fc42dbf6248c4ffb6336e80c5
programmerbobsmith <105533434+programmerbobsmith@users.noreply.github.com>
2022-08-02 15:44:15 -0500

    Add files via upload

---
+++ src/gfx/chem_icon.h
@@ -0,0 +1,18 @@
+#ifndef chem_icon_include_file
+#define chem_icon_include_file
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define chem_icon_width 20
+#define chem_icon_height 20
+#define chem_icon_size 402
+#define chem_icon ((gfx_sprite_t*)chem_icon_data)
+extern unsigned char chem_icon_data[402];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/gfx/menuapple.h
@@ -0,0 +1,18 @@
+#ifndef menuapple_include_file
+#define menuapple_include_file
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define menuapple_width 11
+#define menuapple_height 11
+#define menuapple_size 123
+#define menuapple ((gfx_sprite_t*)menuapple_data)
+extern unsigned char menuapple_data[123];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
